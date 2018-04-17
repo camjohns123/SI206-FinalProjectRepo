566140ae9408e3aaf42c9dd667d8ff3e38c5cbb7
Cami <camjohns@umich.edu>
2018-04-16 21:56:51 -0400

    countries file

---
+++ countries_file.py
@@ -0,0 +1,3 @@
+countries_list = ['australia','cambodia','china','costa rica','cuba','dominican republic',
+'fiji islands','laos','mongolia','morocco','myanmar','mystery destination','peru',
+'tanzania','thailand','united states','vietnam', 'gap']
